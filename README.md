0e6b493271b00474a580f00f5844c8b86e9d9fca
Hajime Yamasaki Vukelic <hi@devi.us>
2022-12-19 18:35:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Yet another TODO app written in plain JavaScript
+
+TODO app is a good basic application that demonstrates a lot of the key 
+concepts in web application development. This particular example focuses on 
+the use of forms APIs to represent the entire application sate entirely in 
+the DOM.
+
+## Running the example
+
+The example can be opened in your browser normally without requiring any 
+development server. You can also use the HTML preview feature in your IDE or 
+editor if one is available.
+
+## Screenshot
+
+![Screenshot of the vanilla TODO app](./screenshot.png)
+
+## Concepts
+
+Here are some of the concepts that this applications demonstrates.
+
+### DOM-infused state
+
+State is stored in and represented by the DOM state. We do not store state any
+JavaScript variables but as properties/attributes on DOM nodes. This way, we
+do not need to perform additional work to synchronize the UI and internal
+application sate, as those are on and the same. The DOM becomes the single
+source of truth for the application.
+
+### Use of `HTMLFormControlsCollection` interface
+
+Every `<form>` and `<fieldset>` element in the DOM will have an `elements` 
+property that implements the 
+[`HTMLFormControlsCollection` interface](https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormControlsCollection).
+This is used throughout the app to quickly access the named inputs, as opposed 
+to using `Element.queryString()` and `Element.queryStringAll()` methods.
+
+### Drag-and-drop ordering
+
+Each fieldset represents a task and can be dragged to change the task order. 
+We use the [HTML drag and drop API](https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API) 
+for this.
+
+### Use of `innerHTML` for rendering elements
+
+To render the fieldset contents we use the `innerHTML` property. This is 
+widely considered an unsafe practice, but it is actually only unsafe in 
+cases where user input is directly inserted into the markup. We use an 
+`escapeHTML` function to clean the input before inserting into the HTML.
---
+++ client.js
@@ -0,0 +1,219 @@
+{
+  'use strict'
+
+  let
+    $tasks = document.getElementById('tasks'),
+    $addTask = document.getElementById('add-task'),
+    $clearCompleted = document.getElementById('clear-completed'),
+    $dragGhost = Object.assign(document.createElement('div'), { hidden: true })
+
+  let
+    toIterable = $nodeOrCollection => {
+      if (!$nodeOrCollection) return []
+      if ('length' in $nodeOrCollection) return $nodeOrCollection
+      return [$nodeOrCollection]
+    },
+    debounce = (fn, t = 100) => {
+      let timeout
+      return (...args) => {
+        clearTimeout(timeout)
+        timeout = setTimeout(fn, t, ...args)
+      }
+    },
+    escapeHTML = s => s.replace(/</g, '&lt;').replace(/"/g, '&quot;'),
+    startFLIP = $ => {
+      // More about FLIP animation:
+      //
+      // https://css-tricks.com/animating-layouts-with-the-flip-technique/
+
+      let initialY = $.offsetTop
+      return {
+        initialY, // we also return the initial offset as that's used to
+                  // calculate whether the dragged task is above or below
+                  // the target
+        run: () => {
+          let
+            finalY = $.offsetTop,
+            animation = $.animate([
+              { transform: `translateY(${initialY - finalY}px` },
+              { transform: 'none' },
+            ], { duration: 200 })
+          // Mark the element as animating so that we can prevent multiple
+          // animation for the same element from starting in parallel
+          $.isAnimating = true
+          // We return a promise that resolves once the animation ends. This
+          // gives us synchronization between multiple animations for free via
+          // Promise.all().
+          return new Promise(res => {
+            animation.addEventListener('finish', () => {
+              delete $.isAnimating
+              res()
+            })
+          })
+        },
+      }
+    }
+
+  let
+    getNextId = () => {
+      let lastId = 0
+      // More about the 'elements' property:
+      // https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormControlsCollection
+      for (let $ of toIterable($tasks.elements.id))
+        lastId = Math.max(lastId, $.value) // NB: Math.max() coerces strings into numbers
+      return lastId + 1
+    },
+    createTask = ({
+      title = '',
+      id = getNextId(),
+      isCompleted = false,
+    }) =>
+      Object.assign(document.createElement('fieldset'), {
+        draggable: true,
+        tabIndex: 0, // allow keyboard focusing for ordering using keyboard shortcuts
+        name: 'task', // this allows us to iterate all fieldsets in the form using HTMLFormElements.elements.task
+        innerHTML: `
+          <div>
+            <input name="id" type="hidden" value="${id}">
+            
+            <label>
+              <span>Task:</span>
+              <input name="title" type="text" required value="${escapeHTML(title)}">
+            </label>
+            
+            <label>
+              <input name="completed" type="checkbox" ${isCompleted ? 'checked' : ''}>
+              <span>Completed</span>
+            </label>
+          </div>
+          
+          <button type="button" value="delete">Delete</button>
+        `,
+      }),
+    clearCompleted = () => {
+      for (let $ of $tasks.querySelectorAll('input[name=completed]:checked'))
+        $.closest('fieldset').remove()
+    },
+    swapElements = ($task, $target) => {
+      let
+        taskFLIP = startFLIP($task),
+        targetFLIP = startFLIP($target)
+
+      if (taskFLIP.initialY > targetFLIP.initialY) $tasks.insertBefore($task, $target)
+      else if ($target.nextElementSibling) $tasks.insertBefore($task, $target.nextElementSibling)
+      else $tasks.append($task)
+
+      return Promise.all([taskFLIP.run(), targetFLIP.run()])
+    },
+    markTaskAsDragging = $task => {
+      $tasks.$draggedTask = $task
+      $task.dataset.dragging = true
+    },
+    markFormAsDragging = () => {
+      $tasks.dataset.dragging = true
+    },
+    unmarkDraggedTaskAsDragging = () => {
+      if ($tasks.$draggedTask) {
+        delete $tasks.$draggedTask.dataset.dragging
+        delete $tasks.$draggedTask
+      }
+    },
+    unmarkFormAsDragging = () => {
+      delete $tasks.dataset.dragging
+    },
+    quickSwapTasks = ($task, $target) => {
+      markTaskAsDragging($task)
+      swapElements($task, $target)
+        .then(unmarkDraggedTaskAsDragging)
+      $task.focus()
+    },
+    toggleClearCompleted = () => {
+      $clearCompleted.hidden = !$tasks.children.length
+    },
+    deleteTask = $task => {
+      $task.nextElementSibling?.focus()
+      $task.remove()
+      storeTasks()
+      toggleClearCompleted()
+    }
+
+  let
+    storeTasks = () => {
+      let data = []
+      // More about the 'elements' property:
+      // https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormControlsCollection
+      for (let $fieldset of toIterable($tasks.elements.task))
+        data.push({
+          id: $fieldset.elements.id.value,
+          title: $fieldset.elements.title.value,
+          isCompleted: $fieldset.elements.completed.checked, // for checkboxes we use .checked rather than .value
+        })
+      localStorage.tasks = JSON.stringify(data)
+    },
+    loadTasks = () => {
+      let data = JSON.parse(localStorage.tasks ?? '[]')
+      for (let task of data) $tasks.append(createTask(task))
+    }
+
+  $tasks.onclick = ev => {
+    let $del = ev.target.closest('button[value=delete]')
+    if ($del) deleteTask($del.closest('fieldset'))
+  }
+  $tasks.oninput = $tasks.onchange = debounce(storeTasks, 300)
+  $tasks.ondragstart = ev => {
+    markTaskAsDragging(ev.target)
+    markFormAsDragging()
+    ev.dataTransfer.setDragImage($dragGhost, 0, 0)
+  }
+  $tasks.ondragover = ev => {
+    if (!ev.target.matches('fieldset')) return
+
+    let
+      $task = $tasks.$draggedTask,
+      $target = ev.target
+
+    if ($task.isAnimating) return
+    if ($task === $target) return
+    swapElements($task, $target)
+  }
+  $tasks.ondragend = ev => {
+    unmarkDraggedTaskAsDragging()
+    unmarkFormAsDragging()
+    storeTasks()
+  }
+  $tasks.onkeydown = ev => {
+    if (!ev.target.matches('fieldset')) return
+
+    let $task = ev.target
+
+    switch (ev.code) {
+      case 'ArrowUp':
+        if (!$task.previousElementSibling) return
+        quickSwapTasks($task, $task.previousElementSibling)
+        break
+      case 'ArrowDown':
+        if (!$task.nextElementSibling) return
+        quickSwapTasks($task, $task.nextElementSibling)
+        break
+      case 'Delete':
+        $task.nextElementSibling?.focus()
+        $task.remove()
+        storeTasks()
+        toggleClearCompleted()
+        break
+    }
+  }
+
+  $addTask.onclick = () => {
+    let $task = createTask({})
+    $tasks.append($task)
+    $task.querySelector('[name=title]').focus()
+    storeTasks()
+    toggleClearCompleted()
+  }
+  $clearCompleted.onclick = clearCompleted
+
+  loadTasks()
+
+  document.querySelector('main').hidden = false
+}
